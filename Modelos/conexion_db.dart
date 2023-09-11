cc1d4a27b97c749aa874aa26ffa04f9c228d7483
mxstikr0w <126942746+archkr0w@users.noreply.github.com>
2023-09-10 22:30:11 -0300

    Update conexion_db.dart
    Conexion a la BD de PostgreSQL

--- Modelos/conexion_db.dart
+++ Modelos/conexion_db.dart
@@ -1,8 +1,8 @@
 import 'package:postgres/postgres.dart';
 
 Future<PostgreSQLConnection> conn() async {
-  final db = PostgreSQLConnection("10.0.2.2", 5432, "new_garden",
-      username: "postgres", password: "123456");
+  final db = PostgreSQLConnection("IP", "PORT", "new_garden",
+      username: "(usuario)", password: "1234567847348");
 
   await db.open();
 
